33286733c35cdd3a82f7e6c32cecdd10a5eb638b
ThiagoDiasV <thiago76ers@gmail.com>
2020-02-28 09:46:35 -0300

    adding audio scraping feature

--- scraper.py
+++ scraper.py
@@ -5,10 +5,11 @@ from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 from sys import argv
 from typing import List
-from ipdb import set_trace
 import csv
 from string import ascii_letters, digits
 import os
+from colorama import Fore
+from random import choice
 
 
 def create_webdriver_instance() -> WebDriver:
@@ -77,13 +78,28 @@ def get_images_from_cards(card: WebElement, card_side: str) -> str:
         return ""
 
 
-def get_card_text(card: WebElement, card_side: str) -> str:
+def get_audio_from_cards(card:WebElement, card_side:str) -> str:
+    """
+    If a card element has audio with it this function will get this audio tag
+    """
+    try:
+        parent_element = card.find_element_by_class_name(card_side)
+        audio_element = parent_element.find_element_by_tag_name("audio")
+        audio_source_element = audio_element.find_element_by_tag_name("source")
+        src_audio = audio_source_element.get_attribute("src")
+        return f'\n<audio controls><source src="{src_audio} type="audio/mpeg"></audio><br>'
+    except NoSuchElementException:
+        return ""
+
+
+def get_card_text_img_audio_data(card: WebElement, card_side: str) -> str:
     """
     Get the card text
     """
     card_img_tag = get_images_from_cards(card, card_side)
+    card_audio_tag = get_audio_from_cards(card, card_side)
     card_info = card.find_element_by_class_name(card_side)
-    card_text = card_info.text + "\n" + card_img_tag
+    card_text = card_audio_tag + "\n" + card_info.text.replace("Play", "") + "\n" + card_img_tag
     return card_text
 
 
@@ -110,7 +126,7 @@ def get_cards_info_of_deck(chrome_webdriver: WebDriver, deck: WebElement):
             "preview-card-table"
         )
         cards_list = cards_window_selection.find_elements_by_class_name("preview-card")
-        csv_file_name = get_file_name_for_csv_files(chrome_webdriver)
+        csv_file_name = get_file_name_for_csv_files(chrome_webdriver).replace("Preview", "").strip()
         path = os.getcwd()
         path_csv = f"{path}/csv_sem_utf8_delimiter_virgula/"
         try:
@@ -120,9 +136,13 @@ def get_cards_info_of_deck(chrome_webdriver: WebDriver, deck: WebElement):
         with open(f"{path_csv}{csv_file_name}.csv", "w", newline="") as csv_file:
             writer = csv.writer(csv_file, delimiter=",")
             for card in cards_list:
-                front_info = get_card_text(card, "front")
-                back_info = get_card_text(card, "back")
+                front_info = get_card_text_img_audio_data(card, "front")
+                back_info = get_card_text_img_audio_data(card, "back")
                 writer.writerow([front_info, back_info])
+        color_options = [
+                Fore.GREEN, Fore.MAGENTA, Fore.CYAN, Fore.BLUE, Fore.YELLOW
+            ]
+        print(choice(color_options) + f"{csv_file_name}.csv salvo!")
 
         chrome_webdriver.find_element_by_class_name("close-button").click()
     except NoSuchElementException:
@@ -149,7 +169,7 @@ def main():
                 sleep(5)
                 get_cards_info_of_deck(chrome, deck)
 
-    print("Terminei!")
+    print(Fore.GREEN + "Terminei!")
     sleep(60)
 
 
